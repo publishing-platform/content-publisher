f8ed3e0a4b909294da5d6718d48f2d04f2824fb5
neilha <neilharrold@gmail.com>
2025-10-31 09:47:54 +0000

    use dartsass in dev, adds initial documents routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
 
   root to: redirect("/documents")
+
+  resources :documents, only: %i[index new create]  
 end
