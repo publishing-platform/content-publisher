2165526c1d842bcdd8747ccd65ae5e0d0af37347
neilha <neilharrold@gmail.com>
2025-11-07 15:59:57 +0000

    adds nav and auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  include PublishingPlatform::SSO::ControllerMethods
+  include EditionAssertions
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :authenticate_user!
+
+  rescue_from EditionAssertions::StateError do |e|
+    Rails.logger.warn(e.message)
+    redirect_to document_path(e.edition.document)
+  end
 end
