e2c6d689a9f2a12f8e0d52a4e189775b8a0df8f5
neilha <neilharrold@gmail.com>
2025-11-13 09:54:41 +0000

    adds checkers for image metadata and image upload

---
+++ spec/lib/requirements/form/image_metadata_checker_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe Requirements::Form::ImageMetadataChecker do
+  describe ".call" do
+    it "returns no issues if there are none" do
+      issues = described_class.call(alt_text: "something")
+      expect(issues).to be_empty
+    end
+
+    it "returns an issue if the alt text is too long" do
+      max_length = Requirements::Form::ImageMetadataChecker::ALT_TEXT_MAX_LENGTH
+      issues = described_class.call(alt_text: "a" * (max_length + 1))
+      expect(issues).to have_issue(:image_alt_text, :too_long, max_length:)
+    end
+
+    it "returns an issue if the caption is too long" do
+      max_length = Requirements::Form::ImageMetadataChecker::CAPTION_MAX_LENGTH
+      issues = described_class.call(caption: "a" * (max_length + 1))
+      expect(issues).to have_issue(:image_caption, :too_long, max_length:)
+    end
+
+    it "returns an issue if the credit is too long" do
+      max_length = Requirements::Form::ImageMetadataChecker::CREDIT_MAX_LENGTH
+      issues = described_class.call(credit: "a" * (max_length + 1))
+      expect(issues).to have_issue(:image_credit, :too_long, max_length:)
+    end
+  end
+end
