172d909ae11d87a9f2d303ac2b079aa4cc64c852
neilha <neilharrold@gmail.com>
2025-11-27 15:03:17 +0000

    edit tags

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,35 @@
+class TagsController < ApplicationController
+  rescue_from PublishingPlatformApi::BaseError do |e|
+    PublishingPlatformError.notify(e)
+    render "#{action_name}_api_down", status: :service_unavailable
+  end
+
+  def edit
+    @edition = Edition.find_current(document_id: params[:document_id])
+    assert_edition_state(@edition, &:editable?)
+  end
+
+  def update
+    results = Tags::UpdateInteractor.call(params:, user: current_user)
+    edition, issues, = results.to_h.values_at(:edition, :issues)
+
+    if issues
+      flash.now["requirements"] = {
+        "items" => issues.items(link_options: issues_link_options(edition)),
+      }
+      render :edit,
+             assigns: { edition:, issues: },
+             status: :unprocessable_entity
+    else
+      redirect_to document_path(params[:document_id])
+    end
+  end
+
+private
+
+  def issues_link_options(edition)
+    edition.document_type.tags.each_with_object({}) do |field, memo|
+      memo[field.id.to_sym] = { href: "##{field.id}-field" }
+    end
+  end
+end
---
+++ app/interactors/tags/update_interactor.rb
@@ -0,0 +1,54 @@
+class Tags::UpdateInteractor < ApplicationInteractor
+  delegate :params,
+           :user,
+           :edition,
+           :revision,
+           :issues,
+           to: :context
+
+  def call
+    Edition.transaction do
+      find_and_lock_edition
+      check_for_issues
+      update_revision
+
+      update_edition
+      update_preview
+    end
+  end
+
+private
+
+  def find_and_lock_edition
+    context.edition = Edition.lock.find_current(document_id: params[:document_id])
+    assert_edition_state(edition, &:editable?)
+  end
+
+  def update_revision
+    updater = Versioning::RevisionUpdater.new(edition.revision, user)
+    updater.assign(tags: update_params)
+    context.fail! unless updater.changed?
+    context.revision = updater.next_revision
+  end
+
+  def check_for_issues
+    issues = Requirements::Form::TagsChecker.call(edition, update_params)
+    context.fail!(issues:) if issues.any?
+  end
+
+  def update_edition
+    EditDraftEditionService.call(edition, user, revision:)
+    edition.save!
+  end
+
+  def update_preview
+    # TODO: ?
+    # FailsafeDraftPreviewService.call(edition)
+  end
+
+  def update_params
+    @update_params ||= edition.document_type.tags.reduce({}) do |hash, field|
+      hash.merge!(field.updater_params(edition, params))
+    end
+  end
+end
