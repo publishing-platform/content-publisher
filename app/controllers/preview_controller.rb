10792255f4f9f895446674fe3175fbc05ec82d39
neilha <neilharrold@gmail.com>
2025-11-28 11:05:11 +0000

    preview page

---
+++ app/controllers/preview_controller.rb
@@ -0,0 +1,20 @@
+class PreviewController < ApplicationController
+  def create
+    # result = Preview::CreateInteractor.call(params:, user: current_user)
+    # issues, preview_failed = result.to_h.values_at(:issues, :preview_failed)
+
+    # if issues
+    #   redirect_to document_path(params[:document]), tried_to_preview: true
+    # elsif preview_failed
+    #   redirect_to document_path(params[:document]),
+    #               alert_with_description: t("documents.show.flashes.preview_error")
+    # else
+    #   redirect_to preview_document_path(params[:document])
+    # end
+  end
+
+  def show
+    @edition = Edition.find_current(document_id: params[:document_id])
+    assert_edition_state(@edition, assertion: "not live") { !@edition.live? }
+  end
+end
