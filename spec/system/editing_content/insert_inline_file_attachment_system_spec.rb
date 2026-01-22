9e19ef4d114a75c0aa6b96bd11fed6d3e23f4adf
neilha <neilharrold@gmail.com>
2026-01-22 10:41:11 +0000

    add system specs for inserting inline file attachments, images, and URL previews

---
+++ spec/system/editing_content/insert_inline_file_attachment_system_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe "Insert inline file attachment", type: :system do
+  scenario "without javascript" do
+    given_there_is_an_edition_with_file_attachments
+    when_i_go_to_edit_the_edition
+    and_i_click_to_insert_a_file_attachment
+    and_i_choose_one_of_the_file_attachments
+    then_i_see_the_attachment_markdown_snippet
+    and_i_see_the_attachment_link_markdown_snippet
+  end
+
+  def given_there_is_an_edition_with_file_attachments
+    @file_attachment_revision = create(:file_attachment_revision,
+                                       :on_asset_manager,
+                                       filename: "foo.pdf")
+    @edition = create(:edition,
+                      document_type: build(:document_type, :with_body),
+                      file_attachment_revisions: [@file_attachment_revision])
+  end
+
+  def when_i_go_to_edit_the_edition
+    visit content_path(@edition.document)
+  end
+
+  def and_i_click_to_insert_a_file_attachment
+    click_on "Insert attachment"
+  end
+
+  def and_i_choose_one_of_the_file_attachments
+    switch_to_window(page.windows.last)
+    click_on "Insert attachment"
+  end
+
+  def then_i_see_the_attachment_markdown_snippet
+    snippet = I18n.t("file_attachments.show.attachment_markdown",
+                     filename: @file_attachment_revision.filename)
+    expect(page).to have_content(snippet)
+  end
+
+  def and_i_see_the_attachment_link_markdown_snippet
+    snippet = I18n.t("file_attachments.show.attachment_link_markdown",
+                     filename: @file_attachment_revision.filename)
+    expect(page).to have_content(snippet)
+  end
+end
---
+++ spec/system/editing_content/url_preview_system_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe "Shows a preview of the URL", type: :system do
+  scenario do
+    given_there_is_an_edition
+    when_i_go_to_edit_the_edition
+    and_i_delete_the_title
+    then_i_see_a_prompt_to_enter_a_title
+    and_i_fill_in_the_title
+    then_i_see_a_preview_of_the_url_on_govuk
+  end
+
+  def given_there_is_an_edition
+    title_field = DocumentType::TitleAndBasePathField.new
+    document_type = build :document_type, contents: [title_field]
+    @edition = create(:edition, document_type:)
+    @document_path_prefix = @edition.document_type.path_prefix
+    @document_base_path = @edition.base_path
+  end
+
+  def when_i_go_to_edit_the_edition
+    visit document_path(@edition.document)
+    click_on "Change Content"
+  end
+
+  def and_i_delete_the_title
+    fill_in("title", with: "")
+    page.find("body").click
+  end
+
+  def then_i_see_a_prompt_to_enter_a_title
+    expect(page).to have_content(I18n.t!("content.edit.url_preview.no_title"))
+  end
+
+  def and_i_fill_in_the_title
+    fill_in("title", with: "A great title")
+    page.find("body").native.send_keys :tab
+  end
+
+  def then_i_see_a_preview_of_the_url_on_govuk
+    expect(page).to have_content("www.test.publishing-platform.co.uk#{@document_path_prefix}/a-great-title")
+  end
+end
