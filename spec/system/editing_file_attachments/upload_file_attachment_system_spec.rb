a56cf6e3863d7df4d078d0c8c3f3f93e08f770c6
neilha <neilharrold@gmail.com>
2026-01-22 15:26:37 +0000

    add system specs for uploading file attachments

---
+++ spec/system/editing_file_attachments/upload_file_attachment_system_spec.rb
@@ -0,0 +1,105 @@
+require "rails_helper"
+
+RSpec.describe "Upload file attachment", type: :system do
+  scenario "inline attachment" do
+    given_there_is_an_edition
+    when_i_go_to_edit_the_edition
+    and_i_go_to_insert_an_attachment
+    and_i_select_a_file_to_upload
+    and_i_upload_the_file_attachment
+    then_i_can_insert_the_attachment
+  end
+
+  scenario "featured attachment" do
+    given_there_is_an_edition_that_allows_featured_attachments
+    when_i_visit_the_summary_page
+    and_i_go_to_change_an_attachment
+    and_i_go_to_add_an_attachment
+    and_i_select_a_file_to_upload
+    and_i_enter_attachment_metadata
+    then_i_can_see_the_attachment
+  end
+
+  def given_there_is_an_edition
+    document_type = build(:document_type, :with_body)
+    @edition = create(:edition, document_type:)
+  end
+
+  def given_there_is_an_edition_that_allows_featured_attachments
+    document_type = build(:document_type, attachments: "featured")
+    @edition = create(:edition, document_type:)
+  end
+
+  def when_i_go_to_edit_the_edition
+    visit document_path(@edition.document)
+    click_on "Change Content"
+  end
+
+  def when_i_visit_the_summary_page
+    visit document_path(@edition.document)
+  end
+
+  def and_i_go_to_insert_an_attachment
+    click_on "Insert attachment"
+  end
+
+  def and_i_go_to_change_an_attachment
+    click_on "Change Attachments"
+  end
+
+  def and_i_go_to_add_an_attachment
+    click_on "Upload file attachment"
+  end
+
+  def and_i_select_a_file_to_upload
+    @attachment_filename = "13kb-1-page-attachment.pdf"
+    @title = "A title"
+
+    stub_asset_manager_receives_an_asset(filename: @attachment_filename)
+    stub_publishing_api_put_content(@edition.content_id, {})
+
+    find('form input[type="file"]').set(Rails.root.join(file_fixture(@attachment_filename)))
+    fill_in "title", with: @title
+  end
+
+  def and_i_upload_the_file_attachment
+    click_on "Upload"
+  end
+
+  def and_i_enter_attachment_metadata
+    click_on "Save and continue"
+    unique_ref = "REF"
+    isbn = "9788700631625"
+    @metadata = "Ref: ISBN #{isbn}, #{unique_ref}"
+
+    fill_in "file_attachment[unique_reference]", with: unique_ref
+    fill_in "file_attachment[isbn]", with: isbn
+
+    stub_asset_manager_updates_any_asset
+    click_on "Save"
+  end
+
+  def then_i_can_see_the_attachment
+    file_metadata = "PDF, 13 KB, 1 page"
+
+    within(".gem-c-attachment") do
+      expect(page).to have_content(@title)
+      expect(page).to have_content(file_metadata)
+      expect(page).to have_content(@metadata)
+    end
+  end
+
+  def then_i_can_insert_the_attachment
+    file_metadata = "PDF, 13 KB, 1 page"
+
+    within(".gem-c-attachment") do
+      expect(page).to have_content(@title)
+      expect(page).to have_content(file_metadata)
+    end
+
+    within(".gem-c-attachment-link") do
+      expect(page).to have_content(@title)
+      expect(page).to have_content(file_metadata)
+    end
+  end
+end
