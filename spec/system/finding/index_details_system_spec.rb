c7b96cd3702a94b2224d2480c64b38a8f6588331
neilha <neilharrold@gmail.com>
2026-01-23 15:37:42 +0000

    add system specs for finding documents

---
+++ spec/system/finding/index_details_system_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "Index details", type: :system do
+  before do
+    stub_publishing_api_has_linkables([], document_type: "organisation")
+  end
+
+  scenario do
+    given_there_is_an_edition
+    when_i_visit_the_index_page
+    then_i_can_see_the_edition
+  end
+
+  def given_there_is_an_edition
+    @edition = create(:edition)
+  end
+
+  def when_i_visit_the_index_page
+    puts current_user
+    visit documents_path
+  end
+
+  def then_i_can_see_the_edition
+    expect(page).to have_content(@edition.title)
+    expect(page).to have_content(@edition.document_type.label)
+    expect(page).to have_content(I18n.t!("user_facing_states.draft.name"))
+    expect(page).to have_content(@edition.updated_at.to_fs(:time_on_date))
+  end
+end
