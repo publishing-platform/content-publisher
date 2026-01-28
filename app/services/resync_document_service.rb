99621c3611475c99d5d3b6571dd3b43b773411a5
neilha <neilharrold@gmail.com>
2026-01-28 15:56:04 +0000

    add ResyncDocumentService and corresponding specs for edition synchronization and removal handling

---
+++ app/services/resync_document_service.rb
@@ -0,0 +1,55 @@
+class ResyncDocumentService
+  include Callable
+
+  def initialize(document, **)
+    @document = document
+  end
+
+  def call
+    Edition.transaction do
+      sync_live_edition if live_edition
+      sync_draft_edition if current_edition != live_edition
+    end
+  end
+
+private
+
+  delegate :live_edition,
+           :current_edition,
+           to: :document
+
+  attr_reader :document
+
+  def sync_live_edition
+    live_edition.lock!
+    PreviewDraftEditionService.call(live_edition, republish: true)
+    PublishAssetsService.call(live_edition)
+
+    if live_edition.removed?
+      redirect_or_remove
+    else
+      publish
+    end
+  end
+
+  def sync_draft_edition
+    current_edition.lock!
+    FailsafeDraftPreviewService.call(current_edition)
+  end
+
+  def publish
+    PublishingPlatformApi.publishing_api.publish(live_edition.document.content_id)
+  end
+
+  def redirect_or_remove
+    removal = live_edition.status.details
+    PublishingPlatformApi.publishing_api.unpublish(
+      live_edition.content_id,
+      type: removal.redirect? ? "redirect" : "gone",
+      explanation: removal.explanatory_note,
+      alternative_path: removal.alternative_url,
+      unpublished_at: removal.removed_at,
+      discard_drafts: true,
+    )
+  end
+end
---
+++ spec/services/resync_document_service_spec.rb
@@ -0,0 +1,98 @@
+require "rails_helper"
+
+RSpec.describe ResyncDocumentService do
+  describe ".call" do
+    before do
+      stub_any_publishing_api_publish
+      stub_any_publishing_api_put_content
+    end
+
+    context "when there is no live edition" do
+      let(:edition) { create(:edition) }
+
+      it "doesn't publish the edition" do
+        expect(FailsafeDraftPreviewService).to receive(:call).with(edition)
+        expect(PublishingPlatformApi.publishing_api).not_to receive(:publish)
+        described_class.call(edition.document)
+      end
+    end
+
+    context "when the current edition is live" do
+      let(:edition) { create(:edition, :published) }
+
+      it "avoids synchronising the edition twice" do
+        expect(PreviewDraftEditionService).to receive(:call).once
+        described_class.call(edition.document)
+      end
+
+      it "re-publishes the live edition" do
+        expect(PreviewDraftEditionService).to receive(:call)
+                              .with(edition, republish: true)
+                              .and_call_original
+
+        request = stub_publishing_api_publish(edition.content_id, {})
+        described_class.call(edition.document)
+
+        expect(request).to have_been_requested
+      end
+
+      it "publishes assets to the live stack" do
+        expect(PublishAssetsService).to receive(:call).once.with(edition)
+        described_class.call(edition.document)
+      end
+    end
+
+    context "when the live edition has been removed" do
+      let(:explanation) { "explanation" }
+
+      before do
+        stub_any_publishing_api_unpublish
+      end
+
+      context "with a redirect" do
+        let(:removal) do
+          build(
+            :removal,
+            redirect: true,
+            alternative_url: "/foo/bar",
+            explanatory_note: explanation,
+          )
+        end
+
+        let(:edition) { create(:edition, :removed, removal:) }
+
+        it "removes and redirects the edition" do
+          remove_params = {
+            type: "redirect",
+            explanation:,
+            alternative_path: removal.alternative_url,
+            unpublished_at: removal.removed_at,
+            discard_drafts: true,
+          }
+
+          request = stub_publishing_api_unpublish(edition.content_id, body: remove_params)
+          described_class.call(edition.document)
+
+          expect(request).to have_been_requested
+        end
+      end
+
+      context "without a redirect" do
+        let(:edition) { create(:edition, :removed, removal: build(:removal)) }
+
+        it "removes the edition" do
+          remove_params = {
+            type: "gone",
+            unpublished_at: edition.status.details.removed_at,
+            discard_drafts: true,
+          }
+
+          request = stub_publishing_api_unpublish(edition.content_id, body: remove_params)
+          described_class.call(edition.document)
+
+          expect(request).to have_been_requested
+        end
+      end
+    end
+  end
+end
