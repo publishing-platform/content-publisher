83e80b09c5044b4faa176403100344a823c5040e
neilha <neilharrold@gmail.com>
2026-01-30 20:50:12 +0000

    refactor PreviewAssetService to assign payloads to variables for clarity and updates asset state to draft

--- app/services/preview_asset_service.rb
+++ app/services/preview_asset_service.rb
@@ -22,15 +22,19 @@ private
   attr_reader :edition, :asset
 
   def update_asset(_asset)
-    Payload.new(edition).for_update
+    payload = Payload.new(edition).for_update # rubocop:disable Lint/UselessAssignment
     # TODO
     # PublishingPlatformApi.asset_manager.update_asset(asset.asset_manager_id, payload)
   end
 
   def upload_asset(asset)
-    Payload.new(edition).for_upload(asset)
-    # TODO
+    payload = Payload.new(edition).for_upload(asset) # rubocop:disable Lint/UselessAssignment
+
+    # TODO: uncomment the below when Asset Manager integration is ready
     # upload = PublishingPlatformApi.asset_manager.create_asset(payload)
     # asset.update!(file_url: upload["file_url"], state: :draft)
+
+    # TODO: delete the line below when Asset Manager integration is ready
+    asset.update!(state: :draft)
   end
 end
